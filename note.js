89a0624bb30e8b3ba0323b9ccc4bf71219516aef
radarpok <vdiana.m8@gmail.com>
2020-09-03 19:57:36 +0200

    expanding note.js with actual developer skills to learn

--- note.js
+++ note.js
@@ -1,4 +1,10 @@
-const notes = {};
+const notes = {
+  // napi 1 ora az alabbi linkek tartalmanak tanulasaval, belinkelni a napirendbe!
+};
+const tanulas_youtube_typescript =
+  "https://www.youtube.com/playlist?list=PL6gx4Cwl9DGAKWClAD_iKpNC0bGHxGhcx";
+const tanulas_youtube_webpack =
+  "https://www.youtube.com/playlist?list=PL6gx4Cwl9DGAKWClAD_iKpNC0bGHxGhcx";
 const tanulas_verziokovetes_youtube_the_new_boston =
   "https://www.youtube.com/playlist?list=PL6gx4Cwl9DGAKWClAD_iKpNC0bGHxGhcx";
 const tanulas_verziokovetes_youtube_ben_awad =
