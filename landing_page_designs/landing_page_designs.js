f7a876e86f2f77ae3abfbbc7fa84a5cda9dd6b76
radarpok <vdiana.m8@gmail.com>
2020-12-20 19:35:35 +0100

    adding new projects to git working folder

---
+++ landing_page_designs/landing_page_designs.js
@@ -0,0 +1,26 @@
+const landing_page_designs = {
+    id: 2020,
+    web_pages: [{
+        // interaktiv rendhagyo oldal igenyes designnal
+        1: `https://fremtidensuddannelser.dk/en`,
+        // live circle orbit menu pelda (react-planettel osszeboronalni)
+        2: `https://katchme.fr/`,
+        // kiugro menu pelda ('Contact us')
+        3: `https://twks.ch/en/`
+    }],
+
+    id: 2019,
+    web_pages: [{
+        // menu design es page layout
+        1: `https://globekit.co/`,
+        2: ``,
+        3: ``
+    }],
+
+    id: 2018,
+    web_pages: [{
+        1: ``,
+        2: ``,
+        3: ``
+    }],
+}
