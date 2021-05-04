204c374453686f27f1de839ab5be0dc97eafbcb2
radarpok <vdiana.m8@gmail.com>
2021-05-04 22:29:19 +0200

    making a database about contact options of BME University lecturers

---
+++ bme_oktatok_adatai.js
@@ -0,0 +1,145 @@
+
+export const bme_oktatok = [
+    {
+        id: 1,
+        targy:               'Fizika 1',
+        oktato_1: [
+            {
+                nev:         'Dr. Kornis János',
+                beosztas:    'egyetemi docens',
+                tanszek:     'TTK Fizika Tanszék',
+                szoba:       'F III. 2. em. 17.',
+                e_mail:      'kornis@eik.bme.hu',
+                telefon:     '+36 1 463 1471'
+            }
+        ],
+        oktato_2: [
+            {
+                nev:         'Dr. Papp Zsolt',
+                beosztas:    'egyetemi adjunktus',
+                tanszek:     'TTK Fizika Tanszék',
+                szoba:       'F III. 2. em. 20.',
+                e_mail:      'zsoltpap@gmail.com',
+                telefon:     '+36 1 463 4181' //?
+            }
+        ],
+        oktato_3: [
+            {
+                nev:         'Dr. Varga Gábor',
+                beosztas:    'egyetemi docens',
+                tanszek:     'TTK Fizika Tanszék',
+                szoba:       'F III. 2. em. 16.',
+                e_mail:      'vargag@phy.bme.hu',
+                telefon:     '+36 1 463 4181' //?
+            }
+        ],
+        oktato_4: [
+            {
+                nev:         'Dr. Bokor Nándor',
+                beosztas:    'egyetemi docens',
+                tanszek:     'TTK Fizika Tanszék',
+                szoba:       'F III. 2. em. 12.',
+                e_mail:      'nbokor@eik.bme.hu',
+                telefon:     '+36 1 463 4184'
+            }
+        ],
+    },
+    {
+        id: 2,
+        targy:               'Mérnök leszek',
+        oktato_1: [
+            {
+                nev:         'Dr. Hercegfi Károly',
+                beosztas:    'egyetemi docens, tanszékvezető',
+                tanszek:     'Gazdaság- és Társadalomtudományi Kar; Ergonómia és Pszichológia Tanszék',
+                szoba:       'Q épület A113',
+                e_mail:      'hercegfi.karoly@gtk.bme.hu',
+                telefon:     '+36 1 463 4022'
+            }
+        ],
+        oktato_2: [
+            {
+                nev:         'Dr. Molnár György',
+                beosztas:    'egyetemi docens, tanszékvezető',
+                tanszek:     'Műszaki Pedagógia Tanszék',
+                szoba:       'Q épület A133',
+                e_mail:      'molnar.gy@eik.bme.hu',
+                telefon:     '+36 1 463 2698'
+            }
+        ],
+        oktato_3: [
+            {
+                nev:         'Dr. Németh József',
+                beosztas:    'c. egyetemi tanár',
+                tanszek:     'Műszaki Pedagógia Tanszék',
+                szoba:       'Q épület A134 (10)',
+                e_mail:      'nemethj@eik.bme.hu',
+                telefon:     '+36 1 463 1296'
+            }
+        ],
+        oktato_4: [
+            {
+                nev:         'Dr. Tevesz Gábor',
+                beosztas:    'egyetemi docens',
+                tanszek:     'Automatizálási és Alkalmazott Informatikai Tanszék',
+                szoba:       'Q épület B216',
+                e_mail:      'tevesz@aut.bme.hu',
+                telefon:     '+36 1 463 2881'
+            }
+        ],
+        oktato_5: [
+            {
+                nev:         'Szabó Bálint',
+                beosztas:    'gyetemi tanársegéd',
+                tanszek:     'Ergonómia és Pszichológia Tanszék',
+                szoba:       'Q épület A104',
+                e_mail:      'szabo.balint@gtk.bme.hu',
+                telefon:     '+36 1 463 2104'
+            }
+        ],
+    },
+    {
+        id: 3,
+        targy:               '',
+        oktato_1: [
+            {
+                nev:         '',
+                beosztas:    '',
+                tanszek:     '',
+                szoba:       '',
+                e_mail:      '',
+                telefon:     ''
+            }
+        ],
+        oktato_2: [
+            {
+                nev:         '',
+                beosztas:    '',
+                tanszek:     '',
+                szoba:       '',
+                e_mail:      '',
+                telefon:     ''
+            }
+        ],
+        oktato_3: [
+            {
+                nev:         '',
+                beosztas:    '',
+                tanszek:     '',
+                szoba:       '',
+                e_mail:      '',
+                telefon:     ''
+            }
+        ],
+        oktato_4: [
+            {
+                nev:         '',
+                beosztas:    '',
+                tanszek:     '',
+                szoba:       '',
+                e_mail:      '',
+                telefon:     ''
+            }
+        ],
+    },
+]
